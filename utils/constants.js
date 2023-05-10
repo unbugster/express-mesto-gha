3fb49864ddc3266362d78a2593a76b0291aeee6c
unbugster <unbugster@gmail.com>
2023-05-10 14:41:36 +0300

    Add URL_REGEXP

--- utils/constants.js
+++ utils/constants.js
@@ -6,4 +6,7 @@ const ERROR_CODES = {
   UNAUTHORIZED: 401,
 };
 
-module.exports = { ERROR_CODES };
+/* eslint-disable no-useless-escape */
+const URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+module.exports = { ERROR_CODES, URL_REGEXP };
